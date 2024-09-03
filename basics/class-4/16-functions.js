ab7f79177462ad422b762d8d96a41e12e1dd21dc
Raúl Dóezon <santor3medio@gmail.com>
2024-09-02 18:59:50 -0600

    🔨 Class 4 - File 16. Functions.

---
+++ basics/class-4/16-functions.js
@@ -0,0 +1,99 @@
+// +++++++++ Funciones +++++++++
+
+// Simple
+function myFunc() {
+  console.log("¡Hola, función!");
+}
+
+for (let i = 0; i < 5; i++) {
+  myFunc();
+}
+
+// Con parámetros
+function myFuncWithParams(name) {
+  console.log(`¡Hola, ${name}!`);
+}
+
+myFuncWithParams("Raúl");
+myFuncWithParams("Dóezon");
+
+// Funciones anónimas
+const myFunc2 = function (name) {
+  console.log(`¡Hola, ${name}!`);
+}
+
+myFunc2("Raúl Dóezon");
+
+// Arrow functions
+const myFunc3 = (name) => {
+  console.log(`¡Hola, ${name}!`);
+};
+
+const myFunc4 = (name) => console.log(`¡Hola, ${name}!`);
+
+myFunc3("Raúl Dóezon");
+myFunc4("Raúl Dóezon");
+
+// Parámetros
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(5, 10);
+sum(5);
+sum();
+
+function defaultSum(a = 0, b = 0) {
+  console.log(a + b);
+}
+
+// Por defecto
+defaultSum();
+defaultSum(5);
+defaultSum(5, 10);
+defaultSum(b = 5);
+
+// Retorno de valores
+function mult(a, b) {
+  return a * b;
+}
+
+let result = mult(5, 10);
+console.log(result);
+
+// Funciones anidadas
+function extern() {
+  console.log("Función externa");
+
+  function intern() {
+    console.log("Función interna");
+  }
+
+  intern();
+}
+
+extern();
+// intern(); Error: fuera del scope.
+
+// Funciones de orden superior
+function applyFunc(func, param) {
+  func(param);
+}
+
+applyFunc(myFunc4, "función de orden superior");
+
+// forEach
+let myArray = [1, 2, 3, 4];
+let mySet = new Set(["Raúl", "Dóezon", "rauldoezon", 32, true, "rauldoezon@mail.mx"]);
+let myMap = new Map([
+  ["name", "Raúl"],
+  ["email", "rauldoezon@mail.mx"],
+  ["age", 32],
+]);
+
+myArray.forEach(function (value) {
+  console.log(value);
+});
+myArray.forEach((value) => console.log(value));
+mySet.forEach((value) => console.log(value));
+myMap.forEach((value) => console.log(value));
