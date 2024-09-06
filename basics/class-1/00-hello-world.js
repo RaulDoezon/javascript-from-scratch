166d6a4e85f78c334b8b785667cc76394777f676
Raúl Dóezon <santor3medio@gmail.com>
2024-09-05 21:04:46 -0600

    🔨 Class 1 -  File 00. Move inside the Class 1 folder.

---
+++ basics/class-1/00-hello-world.js
@@ -0,0 +1,30 @@
+// +++++++++ Hello World +++++++++
+
+// Comentario simple
+
+/*
+  Comentario
+  de
+  varias
+  líneas
+*/
+
+console.log("¡Hola, JavaScript!");
+console.log('¡Hola, JavaScript!');
+console.log(`¡Hola JavaScript!`);
+
+// console.log("Comentar una línea de código.");
+/*
+  console.log("Comentar");
+  console.log("varias");
+  console.log("líneas de código.");
+*/
+
+console.log("92");
+console.log(92);
+console.log(31 + 61);
+console.log(61 - 31);
+console.log(31 * 61);
+console.log(61 / 31);
+console.log(61 % 31);
+console.log(3 ** 31);
