5b41c60c70cb8002f8f519f8b83f7f9a49e136e4
Raúl Dóezon <santor3medio@gmail.com>
2024-09-05 21:05:10 -0600

    🔨 Class 1 -  File 01. Move inside the Class 1 folder.

---
+++ basics/class-1/01-variables.js
@@ -0,0 +1,26 @@
+// +++++++++ Variables +++++++++
+
+// var
+var helloWorld1 = "Hello world 1";
+console.log(helloWorld1);
+
+helloWorld1 = "Hello world 1 modificado...";
+console.log(helloWorld1);
+
+// let
+let helloWorld2 = "Hello world 2";
+console.log(helloWorld2);
+
+helloWorld2 = "Hello world 2 modificado...";
+console.log(helloWorld2);
+
+// const
+const helloWorld3 = "Hello world 3";
+console.log(helloWorld3);
+
+/* 
+  Las siguientes líneas provocarán un error en la consola del navegador:
+
+  helloWorld3 = "Hello world 3 modificado...";
+  console.log(helloWorld3);
+*/
