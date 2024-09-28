12bcae7908cde754e38024159ac8eac40134ffba
Raúl Dóezon <santor3medio@gmail.com>
2024-09-27 21:07:27 -0600

    🔨 Class 7 - File 32. Import modules exercises.

---
+++ basics/class-7/exercises/32-import-modules-exercises.js
@@ -0,0 +1,9 @@
+// 1. Importa una función
+
+// 2. Importa una constante
+
+// 3. Importa una clase
+
+// 4. Importa una función, una constante y una clase por defecto (en caso de que lo permita)
+
+// 5. Importa una función, una constante y una clase desde un directorio diferente al anterior
