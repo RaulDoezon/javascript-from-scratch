6e0766e00f6d6667827b983ac67ac423c688b186
Raúl Dóezon <santor3medio@gmail.com>
2024-10-21 20:26:54 -0600

    🔨 Destructuring: Exercises. Remove code of Destructuring Exercises.

--- basics/class-5/21-destructuring-spreading-exercises.js
+++ basics/class-5/21-destructuring-spreading-exercises.js
@@ -1,37 +1,3 @@
-// 1. Usa desestructuración para extraer los dos primeros elementos de un array
-let myArray = ["Azul", "Rojo", "Blanco", "Negro"];
-let [blue, red] = myArray;
-
-console.log(`${blue} y ${red}.`);
-
-// 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
-let [blue2, red2, white, black, green = "Verde"] = myArray;
-
-console.log(`${blue2}, ${red2}, ${white}, ${black} y ${green}.`);
-
-// 3. Usa desestructuración para extraer dos propiedades de un objeto
-let myObject = {
-  myName: "Raúl",
-  age: 32,
-  studies: {
-    level: "Ingeniería en TICs",
-    school: "ITGAM",
-  }
-};
-let {myName, age} = myObject;
-
-console.log(`Soy ${myName} y tengo ${age} años.`);
-
-// 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas a nuevas variables con nombres diferentes
-let {myFirstName = myName, myAge = age} = myObject;
-
-console.log(`Soy ${myFirstName} y tengo ${myAge} años.`);
-
-// 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
-let {studies: {level}, studies: {school}} = myObject;
-
-console.log(`Estudié en el ${school} ${level}.`);
-
 // 6. Usa propagación para combinar dos arrays en uno nuevo
 let array1To5 = [1, 2, 3, 4, 5];
 let array6To10 = [6, 7, 8, 9, 10];
